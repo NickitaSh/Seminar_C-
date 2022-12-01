2b8d42176a7f5e38013465ae7ddbaf2ced6f1c39
nikita <nikita@example.com>
2022-12-01 21:10:49 +0500

    add solution Dz for task 4

---
+++ Sem1Task4Dz/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 4. Ввод- три числа. Вывод- максимальное из них.
+
+//Считываем данные с консоли(вводим три числа)
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+string? num3Line = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if (num1Line != null && num2Line != null && num3Line != null)
+{
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+    int num3 = int.Parse(num3Line);
+
+    //Вводим новую переменную равную значению функции находящей максимальное число.
+    int max = Math.Max(num1, Math.Max(num2, num3));
+
+    //Выводим на консоль максимальное число.
+    Console.WriteLine("max=" + max);
+}
