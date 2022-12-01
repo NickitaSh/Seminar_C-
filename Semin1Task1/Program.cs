b007bd5130ad31753002e0052578007b58cadfb3
nikita <nikita@example.com>
2022-12-01 21:14:14 +0500

    add solution  for task 0,1,3,5,7

--- Semin1Task1/Program.cs
+++ Semin1Task1/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿//Задача 1. Ввод- два числа. Вывод- равно ли первое число квадрату второго.
+
 //Считываем данные с консоли(вводим два числа)
 string? num1Line = Console.ReadLine();
 string? num2Line = Console.ReadLine();
@@ -9,9 +10,9 @@ if (num1Line != null && num2Line != null)
     //Парсим введенное число
     int num1 = int.Parse(num1Line);
     int num2 = int.Parse(num2Line);
-    
+
     //Проверяем равно ли первое число квадрату второго 
-    if (num1 == num2 * num2) 
+    if (num1 == num2 * num2)
     {
         //Если равно, то выводим ответ "Yes"
         Console.WriteLine("Yes");
