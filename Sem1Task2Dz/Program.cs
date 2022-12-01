4fa4dd704da1616bd55ce93b1725c0b69aa47da9
nikita <nikita@example.com>
2022-12-01 21:10:15 +0500

    add solution Dz for task 2

---
+++ Sem1Task2Dz/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2. Ввод- два числа. Вывод- максимальное из них.
+
+//Считываем данные с консоли(вводим два числа)
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if (num1Line != null && num2Line != null)
+{
+    //Вводим новую переменную. Присваеваем ей значение 0.
+    int max = 0;
+
+    //Парсим введенное число
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Сравниваем введенные числа.
+    if (num1 > num2)
+    {
+        //Если верно, то переменной max присваеваем значение первого числа
+        max = num1;
+        Console.WriteLine("max="+max);
+    }
+    else
+    {
+        //Если неверно, то переменной max присваеваем значение второго числа
+        max = num2;
+        Console.WriteLine("max="+max);
+    }
+}
