2b35fd521d13be745fe888668a39981f42c35bc0
nikita <nikita@example.com>
2022-12-02 16:08:02 +0500

    Add solution for Task 0,1,3,5,7

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -13,15 +13,21 @@ if (inputLineNum != null)
     //Присваеваем новой переменной значение -N
     int startNum = inputNum * (-1);
 
-    //Выходное значение
+    //Выходному значению присваеваем пустую строку
     string outLine = string.Empty;
 
+    //Выполняем цикл пока выполняется условие startNum < inputNum
     while (startNum < inputNum)
     {
+        //Меняем выходное значение 
         outLine = outLine + startNum + ", ";
+
+        //Увеличиваем переменную на 1
         //startNum = startNum + 1
         startNum++;
     }
+
+    //Меняем выходное значение 
     outLine = outLine + startNum;
 
     //Выводим данные в консоль
