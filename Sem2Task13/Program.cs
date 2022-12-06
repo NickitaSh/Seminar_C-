899ec4a2e69f4561451a216a58295a61a32615ca
nikita <nikita@example.com>
2022-12-06 13:30:48 +0500

    Add solution Dz for Task 10,13,15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+//Ввод переменной и проверяем на ноль
+int inputNum = int.Parse(Console.ReadLine() ?? "0");
+if (inputNum > 99)
+{
+    //Переводим число в массив символов
+    char[] digit = inputNum.ToString().ToCharArray();
+    Console.WriteLine(digit[2]);
+}
+else
+{
+   Console.WriteLine("В этом числе нет третьей цифры"); 
+}
