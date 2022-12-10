582de71735b17e3942384f7d0f8c262f80f5d946
nikita <nikita@example.com>
2022-12-10 14:51:29 +0500

    Add solution DZ for Task 19,21,23

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод вывода значения "Полиндром" или "Не Полиндром"
+void PrintData(bool res)
+{
+    if (res == true) 
+    {
+        Console.WriteLine("Полиндром");
+    }
+    else 
+    {
+    Console.WriteLine("Не Полиндром");
+    }
+}
+
+//Метод проверки на полиндром
+bool PolinTest(int num)
+{
+bool res = false; 
+res = ((num / 10000 == num % 10 && (num / 1000) % 10 == (num / 10) % 10));
+return res;
+}
+
+
+int num = ReadData("Введите число");
+bool check = PolinTest(num);
+PrintData(check);
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.
+
+//Вывод координат с помощью метода ReadData
+int number = ReadData("Введите число: ");
+
+//Сборка строки чисел в первой стпени с помощью метода LineBuilder
+string outLine = LineBuilder(number, 1);
+
+//Вывод строки с помощью метода PrintData
+PrintData("", outLine);
+
+//Сборка строки чисел во третьей стпени с помощью метода LineBuilder
+outLine = LineBuilder(number, 3);
+
+//Вывод строки с помощью метода PrintData
+PrintData("", outLine);
+
+// Метод вывода данных
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+// Метод сборки строки чисел в n-ой степени
+string LineBuilder(int n, int pow)
+{
+    string line = string.Empty;
+    for (int i = 1; i < n; i++)
+    {
+        line = line + Math.Pow(i, pow) + " ";
+    }
+    line = line + Math.Pow(n, pow);
+    return line;
+}
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
