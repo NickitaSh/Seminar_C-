0412d14df0d86b691fc4f820113e6258983a9a2c
nikita <nikita@example.com>
2022-12-10 14:50:07 +0500

    Add solution for Task 17,18,20,22

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 18. Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+int quter = ReadData("Введите номер четверти");
+PrintAnswer(quter);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод определяет четверть по знакам координат точки
+void PrintAnswer(int num)
+{
+    if (num > 0 && num < 5)
+    {
+        //Присваевание переменной num номера четверти, в зависимости от знака координат
+        if (num == 1) Console.WriteLine("X > 0, Y > 0");
+        if (num == 2) Console.WriteLine("X > 0, Y < 0");
+        if (num == 3) Console.WriteLine("X < 0, Y < 0");
+        if (num == 4) Console.WriteLine("x < 0, Y > 0");
+    }
+    else Console.WriteLine("Такого номера четверти нет!");
+}
---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задача №20
+//Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 2D пространстве.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод расчета расстояния между точками
+double CalculateLength(int x1, int x2, int y1, int y2)
+{
+    double res = 0;
+
+    //Расчет расстояния между точками
+    res = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+
+    //Возвращаем значение расстояния в переменную res
+    return res;
+}
+
+//Метод вывода значения расстояния между точками
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
+//Вывод координат с помощью метода ReadData
+int x1 = ReadData("Введите x1: ");
+int y1 = ReadData("Введите y1: ");
+int x2 = ReadData("Введите x2: ");
+int y2 = ReadData("Введите y2: ");
+
+//Вывод расстояния между точками с помощью метода PrintData
+//(внутри обращение к методу расчета расстояния)
+PrintData("Расстояние между точками ", CalculateLength(x1, y1, x2, y2));
+
+
+
+
