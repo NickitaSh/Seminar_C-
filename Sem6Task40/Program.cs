a0ca5ed254f4101067480b2c039d4695915d7bd8
nikita <nikita@example.com>
2022-12-20 16:49:48 +0500

    Add solution for Task 39,40,42,44

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача №40 
+//Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+
+// int a = ReadData("Введите первое число: ");
+// int b = ReadData("Введите второе число: ");
+// int c = ReadData("Введите третье число: ");
+
+// if (TriangleTest(a, b, c)) PrintData("Может существовать треугольник сo сторонами такой длины.");
+// else PrintData("Треугольник сo сторонами такой длины не существует.");
+
+// //Метод, считывающий данные, введенные пользователем
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+// //Метод, выводящий на печать данные пользователю
+// void PrintData(string res)
+// {
+//     Console.WriteLine(res);
+// }
+
+// Метод проверяет существование треуг. со сторонами a,b,c
+// bool TriangleTest(int a, int b, int c)
+// {
+//     return ((a + b > c) && (a + c > b) && (b + c > a));
+// }
+int A = ReadData("Enter first number: "); //Ввод данных
+int B = ReadData("Enter second number: ");//Ввод данных
+int C = ReadData("Enter third number: "); //Ввод данных
+PrintData ("Result: "  + TriangleTest(A,B,C)); //Печать результата(внутри метод тест на треуг.)
+
+//Метод считывает  данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+// Метод проверяет существование треуг. со сторонами a,b,c
+bool TriangleTest (int a, int b, int c)
+{
+    return ((a+b>c)&& (a+c>b)&& (b+c>a));
+}
---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №42 
+//Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int inputNumber = ReadData("Enter number: ");
+
+PrintData("Result: " + DegToBin(inputNumber));//Конвертация с помощью расчета
+PrintData("Result: " + DecToBinNativ(inputNumber));//Конвертация с помощью сущесвующей функции
+PrintData("Result: " + Convert.ToString(inputNumber, 8));//Конвертация в 8-ную систему исчислений
+PrintData("Result: " + Convert.ToString(inputNumber, 16));//Конвертация в 16-ную систему исчислений
+
+//Метод считывает  данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+//Метод конвертации с помощью расчета
+string DegToBin(int num)
+{
+    string outBin = String.Empty;
+    while (num > 0)
+    {
+        outBin = (num % 2) + outBin;
+        num = num / 2;
+    }
+    return outBin;
+}
+
+
+//Метод конвертации с помощью сущесвующей функции
+string DecToBinNativ(int num)
+{
+    return Convert.ToString(num, 2);
+}
