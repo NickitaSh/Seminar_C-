4e444bc5bdb57ef0aab16e501f97303404e4a67f
nikita <nikita@example.com>
2022-12-20 16:50:50 +0500

    Add solution DZ for Task 41,43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Метод, считывающий данные, введенные пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод, выводящий на печать данные пользователю
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+//Метод считает количество положительных чисел введенных пользователем
+int CountPosNum(int num)
+{
+    int res = 0;
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"Введите число{i + 1}: ");
+        int digit = int.Parse(Console.ReadLine() ?? "0");
+        if (digit > 0) res++;
+    }
+    return res;
+}
+
+int num = ReadData("Введите количество чисел: "); //Ввод количества чисел
+int res = CountPosNum(num); //Расчет количества чисел > 0
+PrintData("Количество чисел > 0 равно:" + res);// Вывод результата
