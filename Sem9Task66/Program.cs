67887c9c9900e12486d40f2f0e50954269f3b0d9
nikita <nikita@example.com>
2022-12-30 12:17:00 +0500

    Add solution DZ for Task 64,66,68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int sumMN)
+{
+    Console.WriteLine(sumMN);
+}
+
+//Метеод для нахождения суммы натуральных элементов в промежутке от M до N
+int SumMNRec(int M, int N)
+{
+    if (M >= N)
+    {
+        return N;
+    }
+    else
+    {
+        return M + SumMNRec(M + 1, N);
+    }
+}
+int M = ReadData("Введите число M: "); // Чтение данных из консоли
+int N = ReadData("Введите число N: "); // Чтение данных из консоли
+int sumMN = SumMNRec(M, N); //Расчет суммы с помощью рекурсии
+PrintResult(sumMN); //Печать результата
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int ackermannFunction)
+{
+    Console.WriteLine(ackermannFunction);
+}
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannFunction(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+    return AckermannFunction(M, N);
+}
+
+int M = ReadData("Введите число M: "); // Чтение данных из консоли
+int N = ReadData("Введите число N: "); // Чтение данных из консоли
+int ackermannFunction = AckermannFunction(M, N); //Вычисление функции Аккермана
+PrintResult(ackermannFunction); //Печать функции Аккермана
